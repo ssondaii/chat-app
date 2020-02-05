130bef1ef3ce25f277cd09e5f83e4b150c2f892b
SonTV8424 <sontv8424@co-well.com.vn>
2020-02-05 16:47:15 +0700

    event when drag and drop

--- public/js/role/drag_drop.js
+++ public/js/role/drag_drop.js
@@ -2,4 +2,92 @@ $( function() {
     $( "#sortable1, #sortable2" ).sortable({
         connectWith: ".connectedSortable"
     }).disableSelection();
+
+    $('#sortable1').draggable({
+        scroll: true,
+        axis: "x",
+        containment: "body",
+        revert: true,
+        helper: "clone",
+        disable: false,
+        start: function( event, ui ) {
+            // $(ui.item).addClass("active-draggable");
+        },
+        drag: function( event, ui ) {
+        },
+        stop:function( event, ui ) {
+            // $(ui.item).removeClass("active-draggable");
+        }
+    });
+
+    $('#sortable2').draggable({
+        scroll: true,
+        axis: "x",
+        containment: "body",
+        revert: true,
+        helper: "clone",
+        disable: false,
+        start: function( event, ui ) {
+            // $(ui.item).addClass("active-draggable");
+        },
+        drag: function( event, ui ) {
+        },
+        stop:function( event, ui ) {
+            // $(ui.item).removeClass("active-draggable");
+        }
+    });
+
+    $('#sortable1').droppable({
+        // accept: "#div",
+        // class: {
+        //     "ui-droppable-active":"ac",
+        //     "ui-droppable-hover":"hv"
+        // },
+        activate: function( event, ui ) {
+            // $(this).css('background','red');
+        },
+        over: function( event, ui ) {
+            // $(this).css('background','yellow');
+        },
+        out: function( event, ui ) {
+            // $(this).css('background','blue');
+        },
+        drop: function( event, ui ) {
+            // $(this).css('background','white');
+        },
+        deactivate: function( event, ui ) {
+            // $(ui.item).css('background','green');
+        },
+    });
+    $('#sortable2').droppable({
+        // accept: "#div",
+        // class: {
+        //     "ui-droppable-active":"ac",
+        //     "ui-droppable-hover":"hv"
+        // },
+        activate: function( event, ui ) {
+            // $(this).css('background','red');
+        },
+        over: function( event, ui ) {
+            // $(this).css('background','yellow');
+        },
+        out: function( event, ui ) {
+            // $(this).css('background','blue');
+        },
+        drop: function( event, ui ) {
+            // $(this).css('background','white');
+        },
+        deactivate: function( event, ui ) {
+            // $(ui.item).css('background','green');
+        },
+    });
+
+    // $('#sortable1').bind("DOMSubtreeModified",function(){
+    //     setTimeout(function() {
+    //         $('#sortable1').find('input').prop('disabled', false);
+    //         $('#sortable2').find('input').prop('disabled', true);
+    //         $('#sortable2').find('input').prop('checked', false);
+    //         resetDefaultExportSize();
+    //     },100);
+    // });
 } );
